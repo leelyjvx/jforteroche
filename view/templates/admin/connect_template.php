e7868af838e72574294e7163d61c40922fdc0cec
leelyjvx <lena.dupontbroutin@gmail.com>
2019-02-13 22:28:56 +0100

    Added a template for admin section

---
+++ view/templates/admin/connect_template.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?= $title ?></title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="../../bootstrap/css/bootstrap-grid.min.css">
+	<link rel="stylesheet" type="text/css" href="../../bootstrap/css/bootstrap-reboot.min.css">
+	<link rel="stylesheet" type="text/css" href="../../bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="../../public/css/admin/connect_template.css">
+</head>
+<body>
+
+	<?= $content ?>
+
+</body>
+</html>
